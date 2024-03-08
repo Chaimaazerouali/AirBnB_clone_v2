33570f452b34f508bb51cf6570b3b512a03e95c8
Chaimaazerouali <zeroualiiichaimaaa@gmail.com>
2024-03-07 16:42:51 -0800

    alx

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,13 +6,18 @@ execute: fab -f 1-pack_web_static.py do_pack
 
 from datetime import datetime
 from fabric.api import *
+
+
 def do_pack():
-    """ generarates .tgz archive from web_static folder """
-    local("mkdir -p versions")
-    now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    r = local("tar -cvzf versions/web_static_{}.tgz ./web_static".
-              format(now), capture=True)
-    if r.succeeded:
-        return ("versions/web_static_{}.tgz".format(now))
+    """
+    making an archive on web_static folder
+    """
+
+    time = datetime.now()
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
     else:
-        return
+        return None
